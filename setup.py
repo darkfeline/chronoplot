6dbf696dd61af90c996012a8f08fd16b881c0ddf
Allen Li <darkfeline@felesatra.moe>
2016-09-11 01:53:57 -0700

    Repackage

--- setup.py
+++ setup.py
@@ -5,6 +5,20 @@ from setuptools import setup, find_packages
 setup(
     name='chronoplot',
     version='0.1.0',
+    description='Timeline maker.',
+    long_description='',
+    keywords='',
+    url='https://github.com/darkfeline/chronoplot',
+    author='Allen Li',
+    author_email='darkfeline@felesatra.moe',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3.5',
+    ],
+
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={
@@ -12,10 +26,4 @@ setup(
             'chronoplot = chronoplot.main:main',
         ],
     },
-
-    author='Allen Li',
-    author_email='darkfeline@felesatra.moe',
-    description='Timeline maker.',
-    license='GPLv3',
-    url='',
 )
